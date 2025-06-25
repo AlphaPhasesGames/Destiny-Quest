4fa87b6fd44efa840ede2a0e371e8275cf23424c
TWINSUN\Gothr <alphaphasesgames@gmail.com>
2025-06-25 21:40:22 +0100

    added scripts

---
+++ Assets/Stage3Scene2LangMan.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using TMPro;
+
+
+namespace Alpha.Phases.Destiny.Quest
+{
+    public class Stage3Scene2LangMan : MonoBehaviour
+    {
+
+        public TextMeshProUGUI text1;
+        public TextMeshProUGUI text2;
+        public TextMeshProUGUI text3;
+
+        public TextMeshProUGUI letter1Head;
+        public TextMeshProUGUI letter1a;
+        public TextMeshProUGUI letter1b;
+        public TextMeshProUGUI letter2Head;
+        public TextMeshProUGUI letter2a;
+        public TextMeshProUGUI letter2b;
+        public TextMeshProUGUI letter3Head;
+        public TextMeshProUGUI letter3a;
+        public TextMeshProUGUI letter3b;
+        public TextMeshProUGUI letter4Head;
+        public TextMeshProUGUI letter4a;
+        public TextMeshProUGUI letter5Head;
+        public TextMeshProUGUI letter5a;
+        public TextMeshProUGUI letter5b;
+
+        public TextMeshProUGUI book1BP1;
+        public TextMeshProUGUI book1BP2;
+        public TextMeshProUGUI book1BP3;
+        public TextMeshProUGUI book1BP4;
+        public TextMeshProUGUI book1BP5;
+        public TextMeshProUGUI book1BP6;
+        public TextMeshProUGUI book2BP1;
+        public TextMeshProUGUI book2BP2;
+        public TextMeshProUGUI book2BP3;
+        public TextMeshProUGUI book2BP4;
+        public TextMeshProUGUI book3BP1;
+        public TextMeshProUGUI book3BP2;
+        public TextMeshProUGUI book3BP3;
+        public TextMeshProUGUI book3BP4;
+        public TextMeshProUGUI book4BP1;
+        public TextMeshProUGUI book4BP2;
+        public TextMeshProUGUI book4BP3;
+        public TextMeshProUGUI book4BP4;
+        public TextMeshProUGUI book5BP1;
+        public TextMeshProUGUI book5BP2;
+        public TextMeshProUGUI book5BP3;
+        public TextMeshProUGUI book5BP4;
+        public TextMeshProUGUI book5BP5;
+        public TextMeshProUGUI book5BP6;
+        private void Awake()
+        {
+            JSONNode defs = SharedState.LanguageDefs;
+            text1.text = defs["stage3Scene2Text1"];
+            text2.text = defs["stage3Scene2Text2"];
+            text3.text = defs["stage3Scene2Text3"];
+
+            letter1Head.text = defs["mexicoLetter1header"];
+            letter1a.text = defs["mexicoLetter1a"];
+            letter1b.text = defs["mexicoLetter1b"];
+            letter2Head.text = defs["mexicoLetter2header"];
+            letter2a.text = defs["mexicoLetter2a"];
+            letter2b.text = defs["mexicoLetter2b"];
+            letter3Head.text = defs["mexicoLetter3header"];
+            letter3a.text = defs["mexicoLetter3a"];
+            letter3b.text = defs["mexicoLetter3b"];
+            letter4Head.text = defs["mexicoLetter4header"];
+            letter4a.text = defs["mexicoLetter4a"];
+            letter5Head.text = defs["mexicoLetter3header"];
+            letter5a.text = defs["mexicoLetter5a"];
+            letter5b.text = defs["mexicoLetter5b"];
+
+        }
+    }
+}
---
+++ Assets/Stage3Scene2LetterManager.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using LoLSDK;
+namespace Alpha.Phases.Destiny.Quest
+{
+    public class Stage3Scene2LetterManager : MonoBehaviour
+    {
+       // public Stage3Scene1TextMan s3TextMan;
+        [Header("Letter Pages")]
+        public GameObject[] letterPages; // Assign all your letter page UI images here
+        public GameObject letterParent;
+        [Header("Navigation")]
+        public Button forwardButton;     // Your forward arrow button
+        public Button backButton;        // Your back arrow button
+
+        public Button page1TTS;
+        public Button page1TTSa;
+        public Button page2TTS;
+        public Button page2TTSa;
+        public Button page3TTS;
+        public Button page3TTSa;
+        public Button page4TTS;
+        public Button page5TTS;
+        public Button page5TTSa;
+
+        private int currentPage = 0;
+        public Button closeButton;
+        private void Start()
+        {
+            forwardButton.onClick.AddListener(OnForwardButtonClicked);
+            backButton.onClick.AddListener(OnBackButtonClicked);
+            closeButton.onClick.AddListener(CloseLetter);
+
+            page1TTS.onClick.AddListener(PlayTTSLetter1);
+            page1TTSa.onClick.AddListener(PlayTTSLetter1a);
+            page2TTS.onClick.AddListener(PlayTTSLetter2);
+            page2TTSa.onClick.AddListener(PlayTTSLetter2a);
+            page3TTS.onClick.AddListener(PlayTTSLetter3);
+            page3TTSa.onClick.AddListener(PlayTTSLetter3a);
+            page4TTS.onClick.AddListener(PlayTTSLetter4);
+            page5TTS.onClick.AddListener(PlayTTSLetter5);
+            page5TTSa.onClick.AddListener(PlayTTSLetter5a);
+
+            ShowPage(0);
+        }
+
+
+
+        public void ShowPage(int index)
+        {
+            currentPage = index;
+
+            // Hide all pages
+            for (int i = 0; i < letterPages.Length; i++)
+                letterPages[i].SetActive(false);
+
+            // Show the current page
+            letterPages[index].SetActive(true);
+
+            // Handle forward button visibility
+            forwardButton.gameObject.SetActive(false);
+            if (index < letterPages.Length - 1)
+                StartCoroutine(EnableForwardButtonAfterDelay(5f));
+
+            // Handle back button visibility (only show on page 1+)
+            backButton.gameObject.SetActive(index > 0);
+        }
+
+        public void OnForwardButtonClicked()
+        {
+            if (currentPage < letterPages.Length - 1)
+            {
+                currentPage++;
+                ShowPage(currentPage);
+            }
+        }
+
+        public void OnBackButtonClicked()
+        {
+            if (currentPage > 0)
+            {
+                currentPage--;
+                ShowPage(currentPage);
+            }
+        }
+
+        private IEnumerator EnableForwardButtonAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            forwardButton.gameObject.SetActive(true);
+        }
+
+        public void CloseLetter()
+        {
+            letterParent.gameObject.SetActive(false);
+          //  s3TextMan.positionChanged = true;
+          //  s3TextMan.arrayPos = 2;
+        }
+
+        public void PlayTTSLetter1()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter1a");
+            Debug.Log("This TTS Worked");
+        }
+
+        public void PlayTTSLetter1a()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter1b");
+            Debug.Log("This TTS Worked");
+        }
+
+        public void PlayTTSLetter2()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter2a");
+            Debug.Log("This TTS Worked");
+        }
+
+        public void PlayTTSLetter2a()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter2b");
+            Debug.Log("This TTS Worked");
+        }
+
+        public void PlayTTSLetter3()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter3a");
+            Debug.Log("This TTS Worked");
+        }
+
+
+        public void PlayTTSLetter3a()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter3b");
+            Debug.Log("This TTS Worked");
+        }
+        public void PlayTTSLetter4()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter4a");
+            Debug.Log("This TTS Worked");
+        }
+
+        public void PlayTTSLetter5()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter5a");
+            Debug.Log("This TTS Worked");
+        }
+
+
+        public void PlayTTSLetter5a()
+        {
+            LOLSDK.Instance.SpeakText("mexicoLetter5b");
+            Debug.Log("This TTS Worked");
+        }
+
+    }
+}
